644e16fbc6fbba328baf2ed6c18ae7dbc48086d1
hsem4717 <hsem4717@gmail.com>
2023-02-04 22:32:36 +0900

    ADD::계산기웹

---
+++ was-practice/src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.example.CustomWebApplicationServer;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new CustomWebApplicationServer(8080).start();
+    }
+}
